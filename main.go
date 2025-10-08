fb108d03adf3dd80b6c910d7b2246cbd1ffe13f4
alvinalzali <alvinamaterasu@gmail.com>
2025-10-08 18:56:09 +0700

    added server and db conn

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+
+	dbHost := os.Getenv("db_host")
+	dbPort, _ := strconv.Atoi(os.Getenv("db_port"))
+	dbUser := os.Getenv("db_user")
+	dbPassword := os.Getenv("db_password")
+	dbName := os.Getenv("db_name")
+
+	db = connectDB(dbUser, dbPassword, dbName, dbHost, dbPort)
+
+	e := echo.New()
+
+	e.Logger.Fatal(e.Start(":8080"))
+
+}
+
+func connectDB(username, password, dbname, host string, port int) *sql.DB {
+	// connect to db
+	connSt := "host=" + host + " port=" + strconv.Itoa(port) + " user=" + username + " password=" + password + " dbname=" + dbname + " sslmode=disable"
+	db, err := sql.Open("postgres", connSt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to DB " + dbname + " successfully on port" + strconv.Itoa(port))
+	return db
+}
